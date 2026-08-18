b671e6454e4893ecabf8d0422a0c74bab124e337
zen918 <davifernandesdasilva88@gmail.com>
2026-08-18 13:48:49 -0300

    termino primeiro projeto MVC-    termino Aula-01

---
+++ Aula-01/Primeiro-MVC/Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC2026;
+
+public class CadastroController : Controller
+{
+    public IActionResult Aluno()
+    {
+        return View();
+    }
+}
+
